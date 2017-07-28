c2c841511e6e73444eead98246e60b6b652c3732
Danny Cho <danny.cho@appfolio.com>
2017-07-27 23:30:34 -0700

    usage of helper for snack

--- app/helpers/snack_helper.rb
+++ app/helpers/snack_helper.rb
@@ -1,2 +1,12 @@
 module SnackHelper
+  def sorted_snacks
+    snacks = Snack.all.map do |snack|
+      {
+        name: snack.name,
+        vote_count_yes: snack.votes.count { |vote| vote.value == 1 },
+        vote_count_no: snack.votes.count { |vote| vote.value == -1 }
+      }
+    end
+    snacks.sort_by { |snack| snack[:vote_count_yes] }.reverse
+  end
 end
