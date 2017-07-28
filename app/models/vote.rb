188f111ff33cfbb3a2e3f885f35ff36eab7bc8e3
Danny Cho <danny.cho@appfolio.com>
2017-07-27 18:17:38 -0700

    check_duplicate method supports updating db entry

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -7,7 +7,8 @@ class Vote < ApplicationRecord
   private
   def check_duplicate
     puts "User id: #{user_id}, Snack id: #{snack_id}"
-    if Vote.find_by(user_id: user_id, snack_id: snack_id)
+    vote = Vote.find_by(user_id: user_id, snack_id: snack_id)
+    if vote && self.id != vote.id
       errors.add(:user, 'Duplicate vote')
     end
   end
