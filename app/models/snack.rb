264f92785a60aebcdc15874cd723cf55ee00bff9
Danny Cho <danny.cho@appfolio.com>
2017-07-27 18:17:03 -0700

    downcase snack name entries to prevent possible duplicates through letter capitalization

--- app/models/snack.rb
+++ app/models/snack.rb
@@ -1,5 +1,6 @@
 class Snack < ApplicationRecord
+  before_validation { |snack| snack.name = snack.name.downcase }
   has_many :votes
+
   validates :name, presence: true, uniqueness: true
-  attr_accessor :store
 end
