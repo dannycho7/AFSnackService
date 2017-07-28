10314a69ac61f3b9c1fcfa78fa25e3c80e0b420c
Danny Cho <danny.cho@appfolio.com>
2017-07-28 01:46:26 -0700

    add slack verify helper

--- app/helpers/slack_helper.rb
+++ app/helpers/slack_helper.rb
@@ -1,2 +1,21 @@
 module SlackHelper
+  def verify_slack_token
+    unless check_command || check_payload
+      render json: {
+        success: false
+      }
+    end
+  end
+
+  private
+
+  def check_payload
+    payload = JSON.parse(params[:payload]) if params[:payload]
+    payload['token'] == ENV['SLACK_VERIFICATION_TOKEN'] if payload
+  end
+
+  def check_command
+    p "Token: #{params[:token] == ENV['SLACK_VERIFICATION_TOKEN']}"
+    params[:token] == ENV['SLACK_VERIFICATION_TOKEN']
+  end
 end
